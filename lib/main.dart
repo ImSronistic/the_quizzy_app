c66298138db041fb5c35e93351f3037c863e6880
Saransh Chauhan <saranshchauhan700@gmail.com>
2021-03-18 16:53:21 +0530

    update questions in main

--- lib/main.dart
+++ lib/main.dart
@@ -27,24 +27,15 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  Question q1 = Question(q: 'The sun raises in the east.', a: true);
-  Question q2 = Question(q: 'Two added to Two is four.', a: true);
-  Question q3 = Question(q: 'The Four multiply with two is six.', a: true);
-  Question q4 = Question(q: 'The English letters starts with \'z\' ', a: true);
+  List<Widget> scoreKeeper = [];
+  List<Question> questionList = [
+    Question(q: 'The sun raises in the east.', a: true),
+    Question(q: 'Two added to Two is four.', a: true),
+    Question(q: 'The Four multiply with two is six.', a: false),
+    Question(q: 'The English letters starts with \'z\' ', a: false),
+  ];
 
-  // List<Widget> scoreKeeper = [];
-  // List<String> questionList = [
-  //   'You can lead a cow down stairs but not up stairs.',
-  //   'Approximately one quarter of human bones are in the feet.',
-  //   'A slug\'s blood is green.',
-  // ];
-  // List<bool> answer = [
-  //   false,
-  //   true,
-  //   true,
-  // ];
-
-  int questionNum = 0;
+  int questionNum = 0; //keep track of question numbers
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -58,7 +49,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionList[questionNum],
+                questionList[questionNum].q,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -83,7 +74,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer = answer[questionNum];
+                bool correctAnswer = questionList[questionNum].a;
                 if (correctAnswer == true) {
                   print('Right!');
                 } else {
